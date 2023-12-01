f08aa446fabf86ae9bf78a2662304a628da49255
kracelh542 <kracelh542@gmail.com>
2023-12-01 09:31:55 +0100

    Empezamos clases ya creadas

---
+++ src/main/java/daw/Baraja.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daw;
+
+/**
+ *
+ * @author khalid
+ */
+public class Baraja {
+    
+    
+    
+    //atributo = array de naipes
+    //constructor por defecte que rellene con todos 
+    //los naipes en total 40
+    
+    
+}
---
+++ src/main/java/daw/Naipe.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daw;
+
+/**
+ *
+ * @author khalid
+ */
+public class Naipe {
+
+    //10tipos de carta del 1 al 10
+    //4 tipos de palo: bastos,espadas,oros
+    
+    int carta;
+    String palo;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
